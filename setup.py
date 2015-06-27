ce664c03e61492cbd81a5ff5a4cea77cedf0b589
Tim Simkins <simkintr@tandj.net>
2015-06-27 18:27:41 -0400

    Updates for Plone 4.3.6

--- setup.py
+++ setup.py
@@ -6,9 +6,8 @@ version = '1.0'
 setup(name='plonetheme.simkintr',
       version=version,
       description="A theme for Tim Simkins' Website",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      long_description=open("README.md").read() + "\n" +
+                       open(os.path.join("HISTORY.md")).read(),
       classifiers=[],
       keywords='',
       author='Tim Simkins',
@@ -21,16 +20,6 @@ setup(name='plonetheme.simkintr',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-
-      [distutils.setup_keywords]
-      paster_plugins = setuptools.dist:assert_string_list
-
-      [egg_info.writers]
-      paster_plugins.txt = setuptools.command.egg_info:write_arg
-      """,
-      paster_plugins = ["ZopeSkel"],
+      entry_points="",
       )
